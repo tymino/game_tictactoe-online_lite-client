fa400ef7c093385d9a65319afc084e6d76a474d0
tymino <d_team-lex@bk.ru>
2025-07-21 14:49:24 +0300

    client folder

--- server/lobby/index.js
+++
@@ -1,8 +0,0 @@
-export const connectedPlayers = {}
-export const lobbyPlayers = {}
-export let waitingPlayer = null
-export const roomConnectKey = '_:room:_'
-
-export const setWaitingPlayer = (socket) => {
-  waitingPlayer = socket
-}
